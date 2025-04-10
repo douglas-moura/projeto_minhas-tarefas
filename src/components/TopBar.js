af3f207c15c7860f0349b59a15f55178cf36a7db
Douglas Moura <100165194+DougSMoura@users.noreply.github.com>
2025-04-10 17:27:01 -0300

    feat: Json-Server adicionado ao projeto
    - biblioteca json-serve adicionado ao projeto
    - fetch de nome no component TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,15 +1,36 @@
-import { SafeAreaView, StyleSheet, View, Text, Image } from "react-native"
+import { SafeAreaView, StyleSheet, Pressable, Text, Image } from "react-native"
 import { layouts, paletaCores } from "../assets/styles/StylesGlobal"
+import { useEffect, useState } from "react"
 
-export default function TopBar() {
+export default function TopBar({navigation}) {  
+    const [usuarioData, setUsuarioData] = useState(null)
+
+    useEffect(() => {
+        const getUsuario = async () => {
+            try {
+                const response = await fetch('http://192.168.15.151:3000/usuarios')
+                const data = await response.json()
+                setUsuarioData(data)        
+            } catch(err) {
+                console.error(err)
+            }
+        }
+
+        getUsuario()
+    }, [])
+
     return (
         <SafeAreaView style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
         }}>
-            <Text style={[layouts.textoTitulo02, {color: paletaCores.branco}]}>Olá, Nome</Text>
-            <Image style={styles.fotoPerfil} source={require('../assets/img/foto-perfil.jpg')} />
+            <Text style={[layouts.textoTitulo02, {color: paletaCores.branco}]}>
+                Olá, {usuarioData ? usuarioData[0].nome : 'Usuário'}
+            </Text>
+            <Pressable onPress={() => navigation.navigate("Perfil")}>
+                <Image style={styles.fotoPerfil} source={require('../assets/img/foto-perfil.jpg')} />
+            </Pressable>
         </SafeAreaView>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,13 @@ import { layouts, paletaCores } from "../assets/styles/StylesGlobal"
 import TarefasLista from "../components/TarefasLista"
 import TopBar from "../components/TopBar"
 
-export default function HomePage() {
+
+
+export default function HomePage({navigation}) {
     return (
         <SafeAreaView style={layouts.pagina}>
             <View style={[layouts.sessao, {backgroundColor: paletaCores.primaria.medio,}]}>
-                <TopBar />
+                <TopBar navigation={navigation} />
             </View>
             <View style={layouts.sessao}>
                 <TarefasLista tituloLista='Minha Tarefas' />
