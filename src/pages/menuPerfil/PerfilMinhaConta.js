92f6c0dc21e6aa8ad30b9d508a6d33ec33c3ac6d
Douglas Moura <100165194+DougSMoura@users.noreply.github.com>
2025-05-09 10:11:02 -0300

    feat: Sessao de beneficios adicionado a pagina de Minha Conta
    - variavel contendo um array com os beneficios
    - funcao para tranforma cada item do array em um elemento de Text
    - FlatList adicionado a pagina

--- src/pages/menuPerfil/PerfilMinhaConta.js
+++ src/pages/menuPerfil/PerfilMinhaConta.js
@@ -1,13 +1,27 @@
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native'
+import { SafeAreaView, Text, View, StyleSheet, FlatList } from 'react-native'
 import { layouts } from '../../assets/styles/StylesGlobal'
 import { layouts as layoutsPerfil } from '../../assets/styles/StylesPaginasPerfil'
 import { paletaCores } from '../../assets/styles/StylesGlobal'
-import { localhost } from '../../../infos_local'
 import { LinearGradient } from 'expo-linear-gradient'
+import Rodape from '../../components/Rodape'
 import Icon from 'react-native-vector-icons/Feather'
 import BotaoVoltar from '../../components/BotaoVoltar'
 
 export default function PerfilMinhaConta({navigation}) {
+    const listaPlanoBeneficios = [
+        { id: '1', descr: '1.000 tarefas simultâneas' },
+        { id: '2', descr: 'Assistente personalizado' },
+        { id: '3', descr: 'Alertas para tarefas inativas' },
+        { id: '4', descr: 'Tags para identificação de tarefas' },
+    ]
+
+    const renderItem = ({ item }) => (
+        <View style={styles.itemBeneficio}>
+            <Icon name="check" size={20} color={paletaCores.primaria.medio} />
+            <Text style={styles.itemBeneficioNome}>{item.descr}</Text>
+        </View>
+    )
+
     return (
         <SafeAreaView style={layouts.pagina}>
             <View style={layouts.sessao}>
@@ -18,9 +32,21 @@ export default function PerfilMinhaConta({navigation}) {
                     end={{ x: 1, y: 1 }} 
                     style={styles.planoBloco}
                 >
-                    <Text style={[layouts.textoTitulo03, styles.planoNome]}>Plano</Text>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}> 
+                        <Icon name="award" size={20} color={paletaCores.branco} />
+                        <Text style={[layouts.textoTitulo03, styles.planoNome]}>Plano</Text>
+                    </View>
                     <Text style={styles.planoValor}>Grátis</Text>
                 </LinearGradient>
+                <View style={[layoutsPerfil.sessao, styles.sessaoBeneficios]}>
+                    <Text style={[layouts.textoTitulo02, {marginBottom: 12}]}>Benefícios</Text>
+                    <FlatList
+                        data={listaPlanoBeneficios} // Array de dados
+                        renderItem={renderItem} // Função para renderizar cada item
+                        keyExtractor={(item) => item.id} // Chave única para cada item
+                    />
+                </View>
+                <Rodape />
             </View>
         </SafeAreaView>
     )
@@ -34,11 +60,29 @@ const styles = StyleSheet.create({
     },
     planoNome: {
         color: paletaCores.branco,
+        marginLeft: 8,
     },
     planoValor: {
         color: paletaCores.branco,
         fontSize: 38,
         fontWeight: 'bold',
-        marginTop: 6,
+        marginTop: 18,
+    },
+    sessaoBeneficios: {
+        marginTop: 16,
+        padding: 28,
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: paletaCores.cinza.claro,
+    },   
+    itemBeneficio: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 8,
+    },
+    itemBeneficioNome: {
+        marginLeft: 12,
+        fontSize: 16,
+        color: paletaCores.cinza.escuro,
     }
 })
